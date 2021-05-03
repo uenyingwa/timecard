8b24425004a898b7c33dd0a98cb4ba419f76fcc4
Uloaku Enyingwa <uloshuwari@gmail.com>
2021-05-03 14:55:20 +0100

    feature(views): add views
    - user is able to clock in and out
    - user is able to edit time spent

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -8,6 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import '../js/bootstrap_js_files'
+// import '../js/modal'
 
 Rails.start()
 Turbolinks.start()
