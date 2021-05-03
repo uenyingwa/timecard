9e631e430003c9a3f26c8f1b820b3d4476f4636b
Uloaku Enyingwa <uloshuwari@gmail.com>
2021-05-03 14:51:57 +0100

    feature(time-entry): add logic for clocking in
    - each user can only see their own entries
    - update time after clock out
    - ability for user to update time entries

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users
-  root to: 'home#index'
+  root to: 'time_entries#index'
+
+  resources :time_entries, only: %i[index edit update show]
+  get '/clock_in', to: 'time_entries#create'
+  get '/clock_out', to: 'time_entries#clock_out'
 end
